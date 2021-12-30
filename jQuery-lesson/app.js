d2c57b301404279edc58024b0c9ddfa6c007e192
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-30 17:49:39 +0900

    [finish]JS-5

--- jQuery-lesson/app.js
+++ jQuery-lesson/app.js
@@ -1,5 +1,32 @@
-$(document).ready(function(){ 
-  // HTMLの読み込みが終わってからjQueryが実行されるように記述する↑jquely ↓プログラムの内容
-$('body').html('<h1>Hello jQuery!!</h1>') ;
+// $(document).ready(function(){ 
+//   // HTMLの読み込みが終わってからjQueryが実行されるように記述する↑jquely ↓プログラムの内容
+// $('body').html('<h1>Hello jQuery!!</h1>') ;
+// });
+// // (セレクタ).メソッド(パラメータ); 処理内容を記述する
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height':'100px'
+//   });
+// });
+// 上からスライド
+// $(function(){
+//   $('.box1').slideDown();
+// });
+// 下からスライド
+// $(function(){
+//   $('.box1').slideUp();
+// });
+// $(function(){
+//   $('.box1').show();
+//   $('.box1').css({'background-color': '#0000FF'});
+// });
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
-// (セレクタ).メソッド(パラメータ); 処理内容を記述する
