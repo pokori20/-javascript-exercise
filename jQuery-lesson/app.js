343e2303f767d895eedb2d20bee66031bee5c852
pokori20 <93557699+pokori20@users.noreply.github.com>
2021-12-31 19:53:19 +0900

    [finish]JS-6

--- jQuery-lesson/app.js
+++ jQuery-lesson/app.js
@@ -21,12 +21,43 @@
 //   $('.box1').show();
 //   $('.box1').css({'background-color': '#0000FF'});
 // });
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp();
+//   });
+// });
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'})
+//   });
+// });
+// // $('.セレクタ名').イベント名(function(){
+//   イベント発生時に行われる処理
+// });
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+// マウスクリックイベントの記入
+// $('.セレクタ名').on('click', function() {
+//   イベント発生時に行われる処理
+// });
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+});
+ $('.box1').mouseout(function(){
+  $('.box1').removeClass('box1-ext')
   });
 });
